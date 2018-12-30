4c2b6e262492fdd9c2a905a91e658e939a9bad1b
Alexander <talexanderd99@gmail.com>
2018-12-29 19:40:51 -0600

    added text baseddisplay method, finished cmd line parsing for basic methods

--- theory.py
+++ theory.py
@@ -1,29 +1,6 @@
-import networkx as nx 
-
-def main():
-	T = Theory()
-	arg = ""
-	while True:
-		arg = input("Knowlengine: ")
-		if arg == "add":
-			print("fook yourself!\n")
-		elif arg == "link":
-			print("fook yourself!\n")
-		elif arg == "rmIdea":
-			print("fook yourself!\n")
-		elif arg == "rmLink":
-			print("fook yourself!\n")
-		elif arg == "display":
-			print("fook yourself!\n")
-		elif arg == "help":
-			print("Commands: add, link, rmIdea, rmLink, display\n")
-		else:
-			print("Unrecognized argument.\n")
-			break
-
-if __name__ == "__main__":
-	main()
-
+import networkx as nx
+#import networkx.drawing as draw
+#import matplotlib.pyplot as plot
 
 class Idea:
 	#constructor for brainstorm theory type
@@ -31,43 +8,135 @@ class Idea:
 		self.name = name
 		self.description = description
 
-	#constructor for If/Then and FOL theory types
-	#argument is the logical operation internal to this idea
-	def __init__(self, name, description, argument):
-		self.name = name
-		self.description = description
-		self.argument = argument
+	def print(self):
+		print("	Name: "+self.name+" Description: "+self.description)
 
 class Theory:
 
 	def __init__(self):
 		self.ideaNetwork = nx.Graph()
 
+	#return the node with the given name
+	#return None if its not there
+	#for later, try to make this O(1) as opposed to O(N)
+	def getNode(self, name):
+		for node in self.ideaNetwork.nodes():
+			if node.name == name:
+				return node
+		return None
+
+	#adds given idea object to theory graph
 	def addIdea(self, idea):
 		self.ideaNetwork.add_node(idea)
 
+	#away and towards are the string names of the nodes to be linked
+	#doesn't support linking non-existent nodes
 	def linkIdeas(self, away, towards):
-		self.ideaNetwork.add_edge(away, towards)
+		awayNode = self.getNode(away)
+		towardsNode = self.getNode(towards)
 
+		if awayNode!=None and towardsNode!=None:
+			self.ideaNetwork.add_edge(awayNode, towardsNode)
+		else:
+			print("Couldn't link the nodes because at least one does not exist!")
+
+	#remove idea by name string
 	def removeIdea(self, idea):
-		self.ideaNetwork.remove_node(idea)
+		ideaNode = self.getNode(idea)
 
+		if ideaNode!=None:
+			self.ideaNetwork.remove_node(ideaNode)
+		else:
+			print("Can't remove non-existent ideas!")
+
+	#remove link by name strings of connected nodes
 	def removeLink(self, away, towards):
-		self.ideaNetwork.remove_edge(away, towards)
+		#first check that both nodes exist
+		awayNode = self.getNode(away)
+		if awayNode==None:
+			print("Start node doesn't exist!")
+			return
+		towardsNode = self.getNode(towards)
+		if towardsNode==None:
+			print("End node doesn't exist!")
+			return
+		
+		#remove edge if it exists, print error otherwise
+		try:	
+			self.ideaNetwork.remove_edge(awayNode, towardsNode)
+		except:
+			print("Link doesn't exist to be removed!")
+
 
 	#display work here
+	#make it look pretty later
 	def display(self):
-		print("eat ass")
+		print("Nodes: ")
+		for idea in self.ideaNetwork.nodes():
+			idea.print()
+		print("\n")
+
+		print("Edges: ")
+		for edge in self.ideaNetwork.edges():
+			output = "	[ "
+			for idea in edge:
+				output+= idea.name + ", "
+			output+="]"
+			print(output)
+		print("\n")
 
 		
 	#loop through graph for a path back to oneself
 	def checkCircularity(self, idea):
-		print("eat ass")
+		cycles = nx.simple_cycles(self.ideaNetwork)
+		print("Cycles: "+cycles+"\n")
 		
 	#see if any set of ideas are disconnected from the rest
 	#returns this set of disconnected nodes if found, null otherwise
 	def findIslands(self):
-		print("eat ass")
+		print("eat ass\n")
 
+#handles cmd line parsing
+def main():
+	t = Theory()
+	arg = ""
+	while True:
+		arg = input("Knowlengine: ")
+		if arg == "add":
+			name = input("Idea name: ")
+			description = input("Idea description: ")
+			t.addIdea(Idea(name, description))
+			print()
 
+		elif arg == "link":
+			ideaA = input("From idea: ")
+			ideaB = input("To idea: ")
+			t.linkIdeas(ideaA, ideaB)
+			print()
+		
+		elif arg == "rmIdea":
+			name = input("Idea to remove: ")
+			t.removeIdea(name)
+			print()
+		
+		elif arg == "rmLink":
+			ideaA = input("Start of link to remove: ")
+			ideaB = input("End of link to remove: ")
+			t.removeLink(ideaA, ideaB)
+			print()
+
+		elif arg == "display":
+			t.display()
+
+		elif arg == "help":
+			print("Commands: add, link, rmIdea, rmLink, display, exit\n")
+
+		elif arg == "exit":
+			break
+
+		else:
+			print("Unrecognized argument.\n")
+
+if __name__ == "__main__":
+	main()
 		
